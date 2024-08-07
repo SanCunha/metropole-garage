9f7b5ae0b9751eee5a85efd1f96c7dd5c2d34ce5
San Cunha <san.silva@aluno.ufop.edu.br>
2024-08-07 17:59:14 -0300

    removing desnecessary comments

--- backend/src/controllers/vehicleController.ts
+++ backend/src/controllers/vehicleController.ts
@@ -4,10 +4,7 @@ import {
   addVehicleInServer,
   removeVehicleInServer,
 } from "../services/vehicleInServer";
-import { vehicleRepository } from "../repositories/vehicleRepository";
-import { VehicleInServer } from "../entities/VehicleInServer";
 import { vehicleInServerRepository } from "../repositories/vehicleInServerRepository";
-import axios from "axios";
 
 export const getVehicles = async (
   req: Request,
@@ -27,31 +24,10 @@ export const spawnVehicle = async (
   res: Response
 ): Promise<void> => {
   const { id } = req.body;
-  // try {
-  //     const vehicle = await spawnVehicleById(vehicleId);
-  //     if (vehicle) {
-  //         res.status(200).json({ message: 'Vehicle spawned successfully', vehicle });
-  //     } else {
-  //         res.status(404).json({ message: 'Vehicle not found' });
-  //     }
-  // } catch (error) {
-  //     res.status(500).json({ message: 'Error spawning vehicle', error });
-  // }
+
   const spawnedVehicle = await updateVehicle(id, { spawned: true });
   await addVehicleInServer(id);
   await logger();
-  try {
-    // Envia a requisição ao servidor FiveM
-    await axios.post("http://0.0.0.0:30120/spawn-vehicle", {
-      vehicleModel: "adder",
-      playerId: 1,
-    });
-
-    // res.status(200).send("Vehicle spawned successfully");
-  } catch (error) {
-    console.error("Failed to spawn vehicle:", error);
-    // res.status(500).send("Failed to spawn vehicle");
-  }
   res
     .status(200)
     .json({ message: "Vehicle spawned successfully", spawnedVehicle });
@@ -74,7 +50,6 @@ const logger = async () => {
   const vehiclesInServer = await vehicleInServerRepository.find({
     relations: ["vehicle_owner_id", "vehicle_plate"],
   });
-  console.log(vehiclesInServer);
   console.log(`Novo Registro: ${new Date()}`);
   console.log(`Carros no servidor: ${vehiclesInServer.length}`);
   console.log(`Modelo -- Placa -- Id_Carro`);
